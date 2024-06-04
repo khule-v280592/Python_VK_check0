377a5d7189ccbbb23961b68f2be5e4deb861da6f
khule-v280592 <khule.vivek92@outlook.com>
2024-06-04 14:26:27 +0530

    first commit

---
+++ utilities/readProperties.py
@@ -0,0 +1,24 @@
+import configparser
+
+'''
+adding configuration to use everytime
+'''
+
+config = configparser.RawConfigParser()
+config.read(".\\Configurations\\config.ini")
+
+class ReadConfig():
+    @staticmethod
+    def getApplicationURL():
+        url=config.get('common info', 'baseURL')
+        return url
+
+    @staticmethod
+    def getUserName():
+        userName = config.get('common info', 'phoneNumber')
+        return userName
+
+    @staticmethod
+    def getPassword():
+        password = config.get('common info','password')
+        return password
